5901d1dface9b7cf7c957ad57825d3533ca962d1
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-01-07 18:02:30 +0000

    Fixing windows build script: always remove build and dist old folders

--- build_settings/win/build.bat
+++ build_settings/win/build.bat
@@ -9,10 +9,22 @@ set PATH=%WINPYDIR%\Lib\site-packages\PyQt4;%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDI
 rem keep nbextensions in Winpython directory, rather then %APPDATA% default
 set JUPYTER_DATA_DIR=%WINPYDIR%\..\settings
 
+set PROJECTNAME="pythonVideoAnnotator"
 set BUILDSETTINGSDIR="%WORKSPACE%\build_settings\win"
 set MAINSCRIPT="%WORKSPACE%\pythonvideoannotator\main.py"
+set BUILDOUTDIR="%WORKSPACE%\build"
+set DISTOUTDIR="%WORKSPACE%\dist"
 
+echo %PROJECTNAME%
 echo %BUILDSETTINGSDIR%
 echo %WORKSPACE%
+echo %MAINSCRIPT%
+echo %BUILDOUTDIR%
+echo %DISTOUTDIR%
 
-pyinstaller --additional-hooks-dir %BUILDSETTINGSDIR%\hooks --name pythonVideoAnnotator --icon %BUILDSETTINGSDIR%\mouse.ico --onefile %MAINSCRIPT%
+@RD /S /Q %BUILDOUTDIR%
+@RD /S /Q %DISTOUTDIR%
+
+rem python setup.py sdist
+
+pyinstaller --additional-hooks-dir %BUILDSETTINGSDIR%\hooks --name %PROJECTNAME% --icon %BUILDSETTINGSDIR%\mouse.ico --onefile %MAINSCRIPT%

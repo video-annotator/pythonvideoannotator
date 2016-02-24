1f1687e190ad3bfcb69080add2da667c1f0ffd36
Ricardo Ribeiro <ricardojvr@gmail.com>
2016-02-24 11:00:24 +0000

    Update hook-visvis.py

--- build_settings/win/hooks/hook-visvis.py
+++ build_settings/win/hooks/hook-visvis.py
@@ -5,7 +5,7 @@ import visvis
 import visvis.core
 import visvis.processing
 
-modules = getIncludes('qt4')
-hiddenimports = tuple([collect_submodules(module) for module in modules]+[collect_submodules('visvis.core')+collect_submodules('visvis.processing')])
+#modules = getIncludes('qt4');[collect_submodules(module) for module in modules]
+hiddenimports = tuple([collect_submodules('visvis.core')+collect_submodules('visvis.processing')])
 
 datas = collect_data_files('visvis', include_py_files=True)

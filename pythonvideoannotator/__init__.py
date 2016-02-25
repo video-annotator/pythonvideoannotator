f5627c63b391cc0b1a02acdaefead93279712e8d
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-02-25 12:37:28 +0000

    Include myconf sample for user defined settings Update setup.py to auto detect version

---
+++ pythonvideoannotator/__init__.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+""" pythonVideoAnnotator
+
+"""
+
+__author__ = "Carlos Mão de Ferro"
+__copyright__ = "Copyright 2016 Champalimaud Foundation"
+__credits__ = "Carlos Mão de Ferro"
+__license__ = "MIT"
+__version__ = "1.0.0.dev1"
+__maintainer__ = ["Ricardo Ribeiro", "Carlos Mão de Ferro"]
+__email__ = ["ricardojvr at gmail.com", "cajomferro at gmail.com"]
+__status__ = "Development"

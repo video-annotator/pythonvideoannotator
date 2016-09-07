339e4109d8485fbd3fea10dd7e8739bba6c206e4
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2016-09-07 16:25:16 +0100

    Updated build settings to support both python2 and python3 builds Comment batch scripts with ::

--- build_settings/win/build.bat
+++ build_settings/win/build.bat
@@ -1,12 +1,27 @@
-set WINPYDIR=C:\Users\swp\Python\WinPython-32bit-2.7.10.3\python-2.7.10
-set WINPYVER=2.7.10.3
-set HOME=%WINPYDIR%\..\settings
-set WINPYARCH="WIN32"
+@echo off
 
-set PATH=%WINPYDIR%\Lib\site-packages\PyQt4;%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%WINPYDIR%\..\tools;
+setlocal enableextensions enabledelayedexpansion
 
-rem keep nbextensions in Winpython directory, rather then %APPDATA% default
-set JUPYTER_DATA_DIR=%WINPYDIR%\..\settings
+rem set /A PYTHON_VERSION=2
+
+IF /I "%PYTHON_VERSION%" EQU "2" (
+	set "WINPYDIR=C:\Users\swp\Python\WinPython-32bit-2.7.10.3\python-2.7.10"
+	set "WINPYVER=2.7.10.3"
+) ELSE (
+	set "WINPYDIR=C:\Users\swp\Python\WinPython-32bit-3.4.3.7\python-3.4.3"
+	set "WINPYVER=3.4.3.7"
+)
+
+set "HOME=%WINPYDIR%\..\settings"
+set "WINPYARCH=WIN32"
+
+set "PATH=%WINPYDIR%\Lib\site-packages\PyQt4;%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%WINPYDIR%\..\tools;"
+
+:: keep nbextensions in Winpython directory, rather then %APPDATA% default
+set "JUPYTER_DATA_DIR=%WINPYDIR%\..\settings"
+
+echo "PYTHON VERSION:"
+python --version
 
 set PROJECTNAME="pythonVideoAnnotator"
 set BUILDSETTINGSDIR="%WORKSPACE%\build_settings\win"
@@ -14,27 +29,25 @@ set MAINSCRIPT="%WORKSPACE%\pythonvideoannotator\__main__.py"
 set BUILDOUTDIR="%WORKSPACE%\build"
 set DISTOUTDIR="%WORKSPACE%\dist"
 
-rem echo %PROJECTNAME%
-rem echo %BUILDSETTINGSDIR%
-rem echo %WORKSPACE%
-rem echo %MAINSCRIPT%
-rem echo %BUILDOUTDIR%
-rem echo %DISTOUTDIR%
-
+:: clean workspace
 @RD /S /Q %BUILDOUTDIR%
 @RD /S /Q %DISTOUTDIR%
 
-pip uninstall -y pyforms
-
-pip install https://github.com/UmSenhorQualquer/pyforms/archive/master.zip
-
-pip show pyforms
-
-rem echo "Running pyinstaller --additional-hooks-dir %BUILDSETTINGSDIR%\hooks --name %PROJECTNAME% --icon %BUILDSETTINGSDIR%\%ICONNAME% --onefile %MAINSCRIPT%"
+:: echo "Running pyinstaller --additional-hooks-dir %BUILDSETTINGSDIR%\hooks --name %PROJECTNAME% --icon %BUILDSETTINGSDIR%\%ICONNAME% --onefile %MAINSCRIPT%"
 
 pyinstaller --additional-hooks-dir %BUILDSETTINGSDIR%\hooks --name %PROJECTNAME% --icon %BUILDSETTINGSDIR%\cf_icon_128x128.ico --onefile %MAINSCRIPT%
 
 pyinstaller --additional-hooks-dir %BUILDSETTINGSDIR%\hooks --name %PROJECTNAME% --icon %BUILDSETTINGSDIR%\cf_icon_128x128.ico %MAINSCRIPT%
 
 
-python %WINPYDIR%\Scripts\zip.py "%WORKSPACE%\dist\pythonVideoAnnotator" "%WORKSPACE%\dist\pythonVideoAnnotator.zip"
+:: python %WINPYDIR%\Scripts\zip.py "%WORKSPACE%\dist\pythonVideoAnnotator" "%WORKSPACE%\dist\pythonVideoAnnotator.zip"
+
+IF %SOURCEFORGE_UPLOAD% EQU true (
+echo "Uploading to SourceForge..."
+
+c:\curl\curl.exe --progress-bar --netrc-file c:\curl_auth\bitbucket_auth.txt -X POST https://api.bitbucket.org/2.0/repositories/fchampalimaud/pythonvideoannotator/downloads -F files=@"%WORKSPACE%\dist\pythonVideoAnnotator.exe" > curl_output.log
+
+c:\curl\curl.exe --progress-bar --netrc-file c:\curl_auth\bitbucket_auth.txt -X POST https://api.bitbucket.org/2.0/repositories/fchampalimaud/pythonvideoannotator/downloads -F files=@"%WORKSPACE%\dist\pythonVideoAnnotator.zip" > curl_output.log
+) ELSE (
+echo "Skipping upload to SourceForge"
+)

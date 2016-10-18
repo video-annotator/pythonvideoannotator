b22a2213ff9fb6190008aedba1c0dac5fde85d9f
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-10-18 22:38:39 +0100

    .

--- build_settings/win/hooks/hook-pythonvideoannotator.py
+++ build_settings/win/hooks/hook-pythonvideoannotator.py
@@ -1,3 +1,5 @@
 # pyforms import app settings.py dinamically so we need to inform pyinstaller
 
 hiddenimports = ['pythonvideoannotator.settings']
+
+datas = [ ('pythonvideoannotator\\resources', 'pythonvideoannotator\\resources'),]

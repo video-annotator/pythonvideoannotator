2948f7dd1b9b82f5889bd0eebba3cc4555535cfb
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-12-29 11:02:27 +0000

    pyforms hook updated with settings

--- build_settings/win/hooks/hook-pyforms.py
+++ build_settings/win/hooks/hook-pyforms.py
@@ -1,5 +1,5 @@
 from PyInstaller.utils.hooks import collect_data_files
 
-hiddenimports = ["pyforms.Controls", "pyforms.gui.settings"]
+hiddenimports = ["pyforms.Controls", "pyforms.gui.settings", "pyforms.settings"]
 
 datas = collect_data_files('pyforms')

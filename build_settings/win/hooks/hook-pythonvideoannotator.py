06a73c16dcb31995b3856db97dc0f96f3aac1985
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-12-29 11:37:07 +0000

    add pythonvideoannotator.resources to hook

--- build_settings/win/hooks/hook-pythonvideoannotator.py
+++ build_settings/win/hooks/hook-pythonvideoannotator.py
@@ -5,12 +5,16 @@ hiddenimports = [
 	'pythonvideoannotator_models_gui.settings',
 	'pythonvideoannotator.resources',
 	'pythonvideoannotator_module_contoursimages',	
+	'pythonvideoannotator_module_contoursimages.module',
 	'pythonvideoannotator_module_contoursimages.settings',
 	'pythonvideoannotator_module_tracking',
+	'pythonvideoannotator_module_tracking.module',
 	'pythonvideoannotator_module_tracking.settings',
 	'pythonvideoannotator_module_timeline',
+	'pythonvideoannotator_module_timeline.module',
 	'pythonvideoannotator_module_timeline.settings',
 	'pythonvideoannotator_module_patheditor',
+	'pythonvideoannotator_module_patheditor.module',
 	'pythonvideoannotator_module_patheditor.settings',
 ]
 

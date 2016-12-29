a1c23cdb31cf8fdf30b164b4b163f78936928cb7
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-12-29 10:49:21 +0000

    hooks updated

--- build_settings/win/hooks/hook-pythonvideoannotator.py
+++ build_settings/win/hooks/hook-pythonvideoannotator.py
@@ -1,5 +1,11 @@
 # pyforms import app settings.py dinamically so we need to inform pyinstaller
 
-hiddenimports = ['pythonvideoannotator.settings']
+hiddenimports = [
+	'pythonvideoannotator.settings',
+	'pythonvideoannotator_module_contoursimages',
+	'pythonvideoannotator_module_tracking',
+	'pythonvideoannotator_module_timeline',
+	'pythonvideoannotator_module_patheditor',
+]
 
 datas = [ ('pythonvideoannotator\\resources', 'pythonvideoannotator\\resources'),]

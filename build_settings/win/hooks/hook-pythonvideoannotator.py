a4c2075f80c821c9874afb1cc41d8804b82fbecc
Ricardo Ribeiro <ricardo.ribeiro@neuro.fchampalimaud.org>
2016-12-29 11:39:49 +0000

    update hooks to include submodules

--- build_settings/win/hooks/hook-pythonvideoannotator.py
+++ build_settings/win/hooks/hook-pythonvideoannotator.py
@@ -1,21 +1,15 @@
+from PyInstaller.utils.hooks import collect_submodules, collect_data_files
+
 # pyforms import app settings.py dinamically so we need to inform pyinstaller
 
 hiddenimports = [
 	'pythonvideoannotator.settings',
 	'pythonvideoannotator_models_gui.settings',
 	'pythonvideoannotator.resources',
-	'pythonvideoannotator_module_contoursimages',	
-	'pythonvideoannotator_module_contoursimages.module',
-	'pythonvideoannotator_module_contoursimages.settings',
-	'pythonvideoannotator_module_tracking',
-	'pythonvideoannotator_module_tracking.module',
-	'pythonvideoannotator_module_tracking.settings',
-	'pythonvideoannotator_module_timeline',
-	'pythonvideoannotator_module_timeline.module',
-	'pythonvideoannotator_module_timeline.settings',
-	'pythonvideoannotator_module_patheditor',
-	'pythonvideoannotator_module_patheditor.module',
-	'pythonvideoannotator_module_patheditor.settings',
-]
+] \
++ collect_submodules('pythonvideoannotator_module_contoursimages') \
++ collect_submodules('pythonvideoannotator_module_tracking') \
++ collect_submodules('pythonvideoannotator_module_timeline') \
++ collect_submodules('pythonvideoannotator_module_patheditor')
 
 datas = [ ('pythonvideoannotator\\resources', 'pythonvideoannotator\\resources'),]

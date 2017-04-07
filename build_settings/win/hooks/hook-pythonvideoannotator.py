6443f94940887f53ca0e0e319eaf3e8788576c70
UmSenhorQualquer <ricardo.ribeiro@neuro.fchampalimaud.org>
2017-04-07 17:57:45 +0100

    .

--- build_settings/win/hooks/hook-pythonvideoannotator.py
+++ build_settings/win/hooks/hook-pythonvideoannotator.py
@@ -19,6 +19,8 @@ hiddenimports = [
 + collect_submodules('pythonvideoannotator_module_distances') \
 + collect_submodules('pythonvideoannotator_module_pathmap') \
 + collect_submodules('pythonvideoannotator_module_importexport') \
++ collect_submodules('pythonvideoannotator_models') \
++ collect_submodules('pythonvideoannotator_models_gui') \
 + collect_submodules('mcvapi') \
 + collect_submodules('mcvgui') \
 + collect_submodules('geometry_designer') \

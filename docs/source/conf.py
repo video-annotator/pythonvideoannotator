3827c257b74d05da8bfc5641bbaecaf114221889
UmSenhorQualquer <ricardo.ribeiro@neuro.fchampalimaud.org>
2017-05-25 15:15:25 +0100

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,16 +99,20 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': True,
+    'navigation_depth': 3
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-	'css_files': ['_static/custom.css'],
-}
+#html_context = {
+#	'css_files': ['_static/custom.css'],
+#}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
@@ -192,84 +196,4 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
-
-
-
-
-
-
-
-#################################################################################
-### THEME CONFIGURATION #########################################################
-#################################################################################
-
-# (Optional) Logo. Should be small enough to fit the navbar (ideally 24x24).
-# Path should be relative to the ``_static`` files directory.
-#html_logo = "my_logo.png"
-
-# Theme options are theme-specific and customize the look and feel of a
-# theme further.
-"""
-html_theme_options = {
-	# Navigation bar title. (Default: ``project`` value)
-	'navbar_title': "Home",
-
-	# Tab name for entire site. (Default: "Site")
-	'navbar_site_name': "Home",
-
-	# A list of tuples containing pages or urls to link to.
-	# Valid tuples should be in the following forms:
-	#    (name, page)                 # a link to a page
-	#    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
-	#    (name, "http://example.com", True) # arbitrary absolute url
-	# Note the "1" or "True" value above as the third argument to indicate
-	# an arbitrary url.
-	'navbar_links': [
-	    ("User's guide", "user-docs/index"),
-	],
-
-	# Render the next and previous page links in navbar. (Default: true)
-	'navbar_sidebarrel': False,
-
-	# Render the current pages TOC in the navbar. (Default: true)
-	'navbar_pagenav': False,
-
-	# Tab name for the current pages TOC. (Default: "Page")
-	'navbar_pagenav_name': "Page",
-
-	# Global TOC depth for "site" navbar tab. (Default: 1)
-	# Switching to -1 shows all levels.
-	'globaltoc_depth': 1,
-
-	# Include hidden TOCs in Site navbar?
-	#
-	# Note: If this is "false", you cannot have mixed ``:hidden:`` and
-	# non-hidden ``toctree`` directives in the same page, or else the build
-	# will break.
-	#
-	# Values: "true" (default) or "false"
-	'globaltoc_includehidden': "true",
-
-	# HTML navbar class (Default: "navbar") to attach to <div> element.
-	# For black navbar, do "navbar navbar-inverse"
-	'navbar_class': "navbar navbar-inverse",
-
-	# Fix navigation bar to top of page?
-	# Values: "true" (default) or "false"
-	'navbar_fixed_top': "true",
-
-	# Location of link to source.
-	# Options are "nav" (default), "footer" or anything else to exclude.
-	'source_link_position': "footer",
-
-	# Bootswatch (http://bootswatch.com/) theme.
-	#
-	# Options are nothing (default) or the name of a valid theme
-	# such as "amelia" or "cosmo".
-	'bootswatch_theme': "united",
-
-	# Choose Bootstrap version.
-	# Values: "3" (default) or "2" (in quotes)
-	'bootstrap_version': "3",
-}"""
+intersphinx_mapping = {'https://docs.python.org/3.5': None}

a79839a25b2b38078b788a363be0168392da7e86
UmSenhorQualquer <ricardo.ribeiro@neuro.fchampalimaud.org>
2017-06-08 17:36:03 +0100

    .

--- pythonvideoannotator/settings.py
+++ pythonvideoannotator/settings.py
@@ -21,12 +21,13 @@ VIDEO_FILE_PATH = None
 CHART_FILE_PATH = None
 PROJECT_PATH    = None
 #PROJECT_PATH    = '/home/ricardo/Downloads/demo-project'
+#PROJECT_PATH    = '/home/ricardo/Downloads/jonathan'
 
 SAVED_GRAPH_FILE_PATH = ""
 SAVED_BONSAI_FILE_PATH = ""
 
 MAIN_WINDOW_GEOMETRY = 50, 200, 1000, 700
-MAIN_WINDOW_GEOMETRY = 1700, 50, 1400, 1000
+#MAIN_WINDOW_GEOMETRY = 1700, 50, 1400, 1000
 
 
 #VIDEO_FILE_PATH = '/home/ricardo/Downloads/fc2_save_2013-10-29-124117-0001.avi'
@@ -34,20 +35,21 @@ MAIN_WINDOW_GEOMETRY = 1700, 50, 1400, 1000
 #CHART_FILE_PATH = '/home/ricardo/Desktop/01Apollo201403210900/01Apollo201403210900_out.csv', 0, 1
 
 PYFORMS_STYLESHEET 			= os.path.join('pythonvideoannotator', 'resources','themes', 'default', 'stylesheet.css')
-PYFORMS_STYLESHEET_LINUX 	= os.path.join('pythonvideoannotator', 'resources','themes', 'default', 'stylesheet_darwin.css')
+#PYFORMS_STYLESHEET_LINUX 	= os.path.join('pythonvideoannotator', 'resources','themes', 'default', 'stylesheet_darwin.css')
 
 
 #MODULES += 'pythonvideoannotator_module_eventsstats'
 #MODULES += 'pythonvideoannotator_module_regionsfilter'
 
+MODULES += 'pythonvideoannotator_module_importexport'
+MODULES += 'pythonvideoannotator_module_virtualobjectgenerator'
+
 MODULES += 'pythonvideoannotator_module_findorientation'
 MODULES += 'pythonvideoannotator_module_motioncounter'
-MODULES += 'pythonvideoannotator_module_importexport'
 MODULES += 'pythonvideoannotator_module_distances'
 MODULES += 'pythonvideoannotator_module_pathmap'
 MODULES += 'pythonvideoannotator_module_smoothpaths'
 MODULES += 'pythonvideoannotator_module_createpaths'
-MODULES += 'pythonvideoannotator_module_virtualobjectgenerator'
 MODULES += 'pythonvideoannotator_module_backgroundfinder'
 MODULES += 'pythonvideoannotator_module_contoursimages'
 MODULES += 'pythonvideoannotator_module_regionsfilter'

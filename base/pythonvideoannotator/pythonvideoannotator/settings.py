5c72aa2919767627922d566c6cc21fb58e6b48b6
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-03-08 16:30:33 +0000

    Add new tracking system

--- base/pythonvideoannotator/pythonvideoannotator/settings.py
+++ base/pythonvideoannotator/pythonvideoannotator/settings.py
@@ -19,11 +19,17 @@ PYFORMS_SILENT_PLUGINS_FINDER = True
 VIDEO_FILE_PATH = None
 CHART_FILE_PATH = None
 PROJECT_PATH    = None
+
+USERSTATS_TIMEOUT_DAYS = 30
+USERSTATS_APP_ID = 'TPYg57bdaMLFlC8c0XHVjKnvDqSzRJrZoQ'
+USERSTATS_URL = "http://stats.cf-sw.org"
+#USERSTATS_URL = "http://localhost:8000"
+
 #PROJECT_PATH    = '/media/ricardo/PEN/project_bad_tracking'
 #PROJECT_PATH    = '/home/ricardo/Downloads/cecilia_movies/movies/20170526/video37_2017-05-26T10_23_51/video-annotator-prj'
 #PROJECT_PATH = '/media/ricardo/b4d07af6-a493-42cb-8f15-9a5d71a0a78a/cecilia_movies/movies/20170518/video21_2017-05-18T10_40_09/video-annotator-prj'
 
-#PROJECT_PATH = '/home/ricardo/Downloads/to-delete'
+PROJECT_PATH = '/home/ricardo/Downloads/to-delete'
 
 SAVED_GRAPH_FILE_PATH 	= ""
 SAVED_BONSAI_FILE_PATH 	= ""
@@ -41,8 +47,10 @@ PYFORMS_STYLESHEET 			= os.path.join( os.path.dirname(__file__), 'resources','th
 
 
 #MODULES += 'pythonvideoannotator_module_eventsstats'
+
 MODULES += 'pythonvideoannotator_module_importexport'
 MODULES += 'pythonvideoannotator_module_virtualobjectgenerator'
+MODULES += 'pythonvideoannotator_module_idtrackerai'
 MODULES += 'pythonvideoannotator_module_findorientation'
 MODULES += 'pythonvideoannotator_module_motioncounter'
 MODULES += 'pythonvideoannotator_module_distances'

9845ec27d7575c0759da96e408fdcab1ab9cc339
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-08-16 14:02:48 +0100

    update version

--- base/pythonvideoannotator/pythonvideoannotator/__init__.py
+++ base/pythonvideoannotator/pythonvideoannotator/__init__.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-__version__ = "3.305.522"
+__version__ = "3.305.524"
 __author__ 		= ["Ricardo Ribeiro", "Carlos Mao de Ferro", "Hugo Cachitas"]
 __credits__ 	= ["Ricardo Ribeiro", "Carlos Mao de Ferro", "Hugo Cachitas"]
 __license__ 	= "Attribution-NonCommercial-ShareAlike 4.0 International"
